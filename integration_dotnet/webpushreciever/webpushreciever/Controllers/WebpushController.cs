eb7bd8e76c1db2fa0149f499aad42cc2e2afceb2
Jeff Synnestvedt <jsynnestvedt@numerex.com>
2013-10-04 13:38:20 -0500

    added .net example for webpush

---
+++ integration_dotnet/webpushreciever/webpushreciever/Controllers/WebpushController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using webpushreciever.Models;
+
+namespace webpushreciever.Controllers
+{
+    public class WebpushController : ApiController
+    {
+
+
+        
+        [HttpGet]
+        [ActionName("system_status")]
+        public bool getStatus()
+        {
+            return true;
+        }
+        
+
+
+
+        [HttpPost]
+        [ActionName("message")]
+        public void processMessage([FromBody]Message message)
+        {
+            System.Console.WriteLine("Got a message " + message.id);
+
+        }
+
+    }
+}
---
+++ integration_dotnet/webpushreciever/webpushreciever/Models/Message.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace webpushreciever.Models
+{
+    public class Message
+    {
+        public string id { get; set; }
+        public DateTime timestamp { get; set; }
+        public string type { get; set; }
+        public Dictionary<string, string> headers { get; set; }
+        public Dictionary<string, string> data { get; set; }
+
+    }
+}
