f9740d5c376154520d35a5a17710c68431a3d162
Andrew Wolverton <awolverton@gmail.com>
2013-10-16 13:06:19 -0500

    initial node js checkin
    project created without reason with nide

---
+++ integration_node/webpushreciever/server_nossl.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var http = require('http');
+var fs = require('fs');
+
+var app = express();
+var auth = express.basicAuth(function(user, pass) {
+    return (user == "andrewisthecoolest" && pass == "andrewisthecoolest");
+},'Authentication required');
+
+app.use(express.logger());
+app.use(express.bodyParser());
+
+app.get('/numerex_services/delivery/system_status', auth, function(req, res) {
+    console.log("System status check");
+    var body = 'true';
+    res.send(body);
+});
+
+app.post('/numerex_services/delivery/message', auth, function(req, res) {
+    console.log('\r\nMessage = ' + JSON.stringify(req.body));
+    var body = 'OK';
+    res.send(body);
+});
+
+http.createServer(app).listen(8080);
+
+console.log('App started')
---
+++ integration_node/webpushreciever/server_ssl.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var https = require('https');
+var http = require('http');
+var fs = require('fs');
+
+var options = {
+    key: fs.readFileSync('privatekey.pem'),
+    cert: fs.readFileSync('certificate.pem')
+};
+
+var app = express();
+var auth = express.basicAuth(function(user, pass) {
+    return (user == "andrewisthecoolest" && pass == "andrewisthecoolest");
+},'Authentication required');
+
+app.use(express.logger());
+app.use(express.bodyParser());
+
+app.get('/numerex_services/delivery/system_status', auth, function(req, res) {
+    console.log("System status check");
+    var body = 'true';
+    res.send(body);
+});
+
+app.post('/numerex_services/delivery/message', auth, function(req, res) {
+    console.log('\r\nMessage = ' + JSON.stringify(req.body));
+    var body = 'OK';
+    res.send(body);
+});
+
+http.createServer(app).listen(80);
+https.createServer(options, app).listen(443);
+
+console.log('App started')
